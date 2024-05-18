182bcd36770219be52f67f080c49e5ba90008a35
SHIVAM SINGH <91686685+Shivams9@users.noreply.github.com>
2024-05-19 04:10:38 +0530

    Update WordCount_inSent.py

--- countSent.py
+++ countSent.py
@@ -1,6 +1,15 @@
-#takes sentense as input and count number of words in it.
-n= input('Enter the Sentense')
+#takes sentence as input and count number of words in it.
+n= input('Enter the Sentence')
 count=0
 for i in n:
     count+=1
-print('Number of Sentense : ',count,end=' ')
+print('Number of Sentence : ',count,end=' ')
+
+
+#takes sentense as input and count number of words in it #Example = Jay Shree Ram (Count=3)
+
+n=input("Enter Sentence :")
+
+word_count=n.split()
+number=len(word_count)
+print("WordsCount in Sentence: ",number)
